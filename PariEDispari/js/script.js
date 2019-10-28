30858b93f6ebb43fc11e0a9a96473a0769759096
Mr2803 <rispoli.mar2803@gmail.com>
2019-10-28 14:51:16 +0100

    test

--- PariEDispari/js/script.js
+++ PariEDispari/js/script.js
@@ -1,18 +1,41 @@
 /* L’utente sceglie pari o dispari e un numero da 1 a 5.
 Generiamo un numero random(sempre da 1 a 5) per il computer.
 Sommiamo i due numeri e dichiariamo chi ha vinto. */
-var sceltaPC;
+//@ts-check
+var sceltaPC , risultato;
 
 var pariDispari = prompt("scegli pari o dispari");
 console.log(pariDispari);
+
 var numero= prompt("scegli un numero da 1 a 5");
 console.log(numero);
-sceltaPC = numPC (5,1);
-console.log(sceltaPc)
 
-function numPc (max, min){
-    var numeropc = Math.floor(Math.random() * (max - min + 1)) + min;
+sceltaPC = numPc();
+console.log(sceltaPC);
+
+risultato = add();
+console.log(risultato)
+
+//BLOCCO FUNZIONI
+
+function numPc (){
+    var numeropc = Math.floor(Math.random() * 5) + 1;
     
     return numeropc; 
     
+}
+
+
+function add(){
+    var somma = numero + sceltaPC;
+
+    if(somma % 2 == 0 && pariDispari == "pari"){
+        return "ha vinto il giocatore";
+    }else if(somma % 2 == 0 && pariDispari == "dispari"){
+        return "ha vinto il pc";
+    }else if(somma % 2 !==0 && pariDispari == "dispari"){
+        return "ha vinto il giocatore";
+    } else if (somma % 2 !== 0 && pariDispari == "pari"){
+        return "ha vinto il pc";
+    }
 }
