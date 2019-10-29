37a646599777b1e88b07edb96d8c5659d9e76ab6
Mr2803 <rispoli.mar2803@gmail.com>
2019-10-29 10:30:55 +0100

    add function in script2 palindromo (bonus)

--- Palindromo/js/script2.js
+++ Palindromo/js/script2.js
@@ -1,17 +1,22 @@
 //Una funzione per capire se la parola è palindroma
+//@ts-check
 
 var parolaUser = prompt("inserisci una parola");
 console.log(parolaUser)
 
-var parolaReverse = '';
+pal(parolaUser);
 
-for(var i = parolaUser.length -1 ; i>=0; i--){
-    parolaReverse += parolaUser[i];
-}
-console.log(parolaReverse)
 
-if(parolaReverse == parolaUser){
-    console.log("è palindroma")
-}else{
-    console.log("non è palindroma")
+function pal(palindroma){
+    var parolaReverse = '';
+
+    for (var i = palindroma.length - 1; i >= 0; i--) {
+        parolaReverse += palindroma[i];
+    }
+
+    if (parolaReverse == palindroma) {
+        console.log("è palindroma")
+    } else {
+        console.log("non è palindroma")
+    }
 }
