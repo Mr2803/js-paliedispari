5d120fced93e1382d04ec02641efbe8a9610a42a
Mr2803 <rispoli.mar2803@gmail.com>
2019-10-29 11:41:25 +0100

    clean code

--- Palindromo/js/script.js
+++ Palindromo/js/script.js
@@ -3,7 +3,7 @@
 var parolaUser = prompt("inserisci una parola");
 
 console.log(parolaUser.split(""));//con il comando SPLIT divido la parola inserita dall'utente per ogni carattere diventa quindi un array (se non inserissi gli apici lo considera come un array con un solo elemento)
-console.log(parolaUser.split("").reverse(""));//uso poi il comando REVERSE(preceduto dal comando split altrimenti non funziona (probabile funzioni solo con array quindi non può dividere una stringa)) che inverte l'ordine dell'array precedentemente creato dal comando split
+console.log(parolaUser.split("").reverse());//uso poi il comando REVERSE(preceduto dal comando split altrimenti non funziona (probabile funzioni solo con array quindi non può dividere una stringa)) che inverte l'ordine dell'array precedentemente creato dal comando split
 console.log(parolaUser.split("").reverse().join(""))//uso il comando JOIN che SENZA VIRGOLETTE mi restituisce l'array come lettere seguite da una virgola,CON le virgolette invece mi restituisce una sola parola
 document.getElementById("palOrNot").innerHTML= "La parola inserita";
 document.getElementById("palindromo").innerHTML = palindromo(parolaUser);
@@ -12,7 +12,7 @@ document.getElementById("palindromo").innerHTML = palindromo(parolaUser);
 
 //BLOCCO FUNZIONI
 function palindromo(pal) {
-    var palin = pal.split("").reverse("").join("");
+    var palin = pal.split("").reverse().join("");
     //creo una variabile d'appoggio che ho chiamato palin e dico che PALIN sarà uguale all'argomento della funzione palindromo(argomento che dopo diventerà la parola inserita dall'utente,nel mio caso) però DIVISA(SPLIT),REVERTATA(REVERSE),UNITA(JOIN)
 
     if (palin === pal) {
--- PariEDispari/js/script.js
+++ PariEDispari/js/script.js
@@ -47,7 +47,3 @@ function parDisp(somma,pariDispari){
     }
 }
 
-
-
-
-/*  */
